3eb97df50d4ecdd57f5881d1a608adf21895aad3
yashmaheshwari0502 <yashmaheshwari0502@gmail.com>
2024-02-22 22:59:37 +0530

    Find the Town Judge

--- 07_997.cpp
+++ 07_997.cpp
@@ -1,3 +1,4 @@
+/*
 #include<iostream>
 #include<vector>
 #include<unordered_map>
@@ -44,6 +45,57 @@ int main(){
 }
 
 
+*/
+
 
 // solution made according to the test cases 
 // solution not optimized
+
+
+
+
+// approach -2
+
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+       vector<int> count(n+1);
+       for(vector<int> &vec:trust){
+        int u=vec[0];
+        int v=vec[1];
+
+        count[u]--;
+        count[v]++;
+       }
+       for(int i=1;i<=n;i++){
+        if(count[i]==n-1){
+            return i;
+        }
+       }
+       return -1;
+    }
+};
+
+
+
+
+int main()
+{
+    int n=4;
+    vector<vector<int>> trust={{1,3},{1,4},{2,3},{2,4},{4,3}};
+    Solution sol;
+    cout<<sol.findJudge(n,trust);
+    return 0;
+    return 0;
+}
+
+
+
+// This solution is provided by codestory with mil
+// my solution is also correct but it is also good and better than mine
